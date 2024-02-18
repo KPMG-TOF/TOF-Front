301dcac4c9a2860b28710e41661539f781375849
hellokms8828 <hellokms8828@naver.com>
2024-02-18 16:28:57 +0900

    Fix: dummy 수정

--- src/sections/user copy/view/user-view.jsx
+++ src/sections/user copy/view/user-view.jsx
@@ -130,15 +130,15 @@ export default function UserPage() {
     try {
 
 
-      // const res = await ref_list();
-      // if (res.data.result === "success") {
-      //   const rfpdumpData =res.data.rfp_list;
+      const res = await ref_list();
+      if (res.data.result === "success") {
+        const rfpdumpData =res.data.rfp_list;
       
-      const rfpdumpData = [
-        { id: '1', file: 'RFP title1', upload_date: '2024.04.02', progress: true},
-        { id: '2', file: 'RFP title2', upload_date: '2024.04.08', progress: true},
-        // 여기에 더 많은 더미 데이터 객체 추가
-            ];
+      // const rfpdumpData = [
+      //   { id: '1', file: 'RFP title1', upload_date: '2024.04.02', progress: true},
+      //   { id: '2', file: 'RFP title2', upload_date: '2024.04.08', progress: true},
+      //   // 여기에 더 많은 더미 데이터 객체 추가
+      //       ];
 
       const rfpdump = rfpdumpData.map(rfp => ({
         no: rfp.id,
@@ -149,8 +149,8 @@ export default function UserPage() {
       }));
 
       return rfpdump;
-    // }
-    // return [];
+    }
+    return [];
     } catch (error) {
       console.error('사용자 데이터를 불러오는 데 실패했습니다.', error);
       return []; // 에러 발생 시 빈 배열 반환
